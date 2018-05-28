dc54cd4ec8a8200086671e31a9d5b17eeae74635
Yin Shanyang <yang@swarm.is>
2018-05-28 18:37:46 +0800

    explorations with responsive grid layouts

--- src/lib/story.js
+++ src/lib/story.js
@@ -1,35 +1,17 @@
 // @flow
 import React from 'react'
+import * as d3 from 'd3'
+import hsluv from 'hsluv'
 import { storiesOf } from '@storybook/react'
 
 storiesOf('Layouts', module)
-  .add('grid', () => {
-    const styles = {
-      grid: {
-        display: 'grid',
-        gridTemplateColumns: 'repeat(auto-fill, minmax(240px, 1fr))',
-        gridGap: 8
-      }
-    }
-
-    return (
-      <div className='w-100 bg-background-80' style={styles.grid}>
-        {
-          Array(20).fill(0).map((_, index) => (
-            <div key={index} className='bg-primary-10 aspect-ratio aspect-ratio--16x9'>{index}</div>
-          ))
-        }
-      </div>
-    )
-  })
   .add('complex grid', () => {
     const styles = {
       application: {
         display: 'grid',
-        // gridTemplateColumns: 'minmax(240px, 1fr) 4fr',
         gridTemplateColumns: 'repeat(auto-fill, minmax(64px, 1fr))',
-        gridGap: 8,
-        padding: 8
+        gridGap: 1,
+        padding: 1
       },
       sidebar: {
         gridColumn: 'span 2'
@@ -39,11 +21,14 @@ storiesOf('Layouts', module)
         gridTemplateColumns: 'repeat(auto-fill, minmax(64px, 1fr))',
         gridAutoRows: 'minmax(64px, 64px)',
         alignContent: 'stretch',
-        gridAutoFlow: 'dense',
-        gridGap: 8,
+        gridAutoFlow: 'row dense',
+        gridGap: 1,
         gridColumn: '3 / content-max'
       }
     }
+
+    const n = ~~(Math.random() * 5000) + 120
+    const c = d3.scaleSequential(d3.interpolateWarm).domain([0, n])
 
     return (
       <div className='w-100 h-100 bg-background-100 f8 ttu tracked' style={styles.application}>
@@ -52,13 +37,14 @@ storiesOf('Layouts', module)
         </div>
         <div className='overflow-auto' style={styles.grid}>
           {
-            Array(~~(Math.random() * 100) + 30).fill(0).map((_, index) => (
+            Array(n).fill(0).map((_, index) => (
               <div
                 key={index}
                 className='bg-background-40 text-reversed-100 pa2'
                 style={{
                   gridColumn: `span ${~~(Math.pow(Math.random(), 3) * 4) + 1}`,
-                  gridRow: `span ${~~(Math.pow(Math.random(), 3) * 4) + 1}`
+                  gridRow: `span ${~~(Math.pow(Math.random(), 3) * 4) + 1}`,
+                  background: c(index)
                 }}
               >
                 {index}
@@ -69,119 +55,144 @@ storiesOf('Layouts', module)
       </div>
     )
   })
-  .add('inline', () => (
+  .add('responsive grid (collapsing grids)', () => {
+    const css = `
+      .application {
+        display: grid;
+        grid-gap: 1px;
+        padding: 1px;
+      }
+      .navigation {}
+      .main {
+        display: grid;
+        grid-gap: 1px;
+        grid-auto-flow: row dense;
+        align-content: stretch;
+        align-items: stretch;
+      }
+
+      .application {
+        grid-template-columns: 1fr;
+        grid-template-rows: 64px 1fr;
+      }
+      .navigation {}
+      .main {
+        overflow: auto
+      }
+
+      @media screen and (min-width: 30em) {
+        .application {}
+        .navigation {}
+        .main {}
+      }
+
+      @media screen and (min-width: 30em) and (max-width: 60em) {
+        .application {
+          grid-template-columns: repeat(auto-fill, minmax(64px, 1fr));
+          grid-template-rows: 1fr;
+        }
+        .navigation {
+          grid-column: span 3;
+        }
+        .main {
+          grid-column: 4 / max-content;
+          overflow: auto;
+        }
+      }
+
+      @media screen and (min-width: 60em) {
+        .application {
+          grid-template-columns: repeat(auto-fill, minmax(64px, 1fr));
+          grid-template-rows: 1fr;
+        }
+        .navigation {
+          grid-column: span 3;
+        }
+        .main {
+          grid-column: 4 / max-content;
+          grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+        }
+        .main>div {
+          overflow: auto
+        }
+      }
+    `
+
+    const n = ~~(Math.random() * 4) + 1
+    return (
+      <div className='application w-100 h-100 bg-background-100 f8 ttu tracked'>
+        <style type='text/css'>{css}</style>
+        <div className='navigation bg-background-40 text-reversed-100 pa2'>
+          navigation: {n}
+        </div>
+        <div className='main'>
+          {
+            Array(n).fill(0)
+              .map(getGrid)
+          }
+        </div>
+      </div>
+    )
+  })
+
+function getGrid () {
+  const n = ~~(Math.random() * 1000) + 120
+  const c = d3.scaleSequential(d3.interpolateWarm).domain([0, n])
+
+  return (
     <div
       style={{
-        padding: 8
+        display: 'grid',
+        gridTemplateColumns: 'repeat(auto-fill, minmax(64px, 1fr))',
+        gridAutoRows: 'minmax(64px, 64px)',
+        alignContent: 'stretch',
+        gridAutoFlow: 'row dense',
+        gridGap: 1
       }}
-      className='h-100 flex flex-wrap items-start content-start'
     >
       {
-        Array(~~(Math.random() * 20) + 20).fill(0).map(() => (
-          <div
-            style={{
-              padding: 8
-            }}
-          >
-            <div
-              className='bg-background-50'
-              style={{
-                height: 4 * 16,
-                width: (~~(Math.random() * 4) + 1) * 3 * 16
-              }}
-            />
-          </div>
+        Array(n).fill(0).map((_, index) => (
+          Math.random() > 0.99
+            ? (
+              <div
+                key={index}
+                className='bg-background-40 text-reversed-100 pa2'
+                style={{
+                  gridColumn: '1 /-1',
+                  gridRow: `span ${Math.ceil((Math.random() * 400 + 64) / 64)}`,
+                  // background: c(index)
+                  background: getGray(c(index))
+                }}
+              >
+                {index}
+              </div>
+            )
+            : (
+              <div
+                key={index}
+                className='bg-background-40 text-reversed-100 pa2'
+                style={{
+                  gridColumn: `span ${~~(Math.pow(Math.random(), 3) * 4) + 1}`,
+                  gridRow: `span ${~~(Math.pow(Math.random(), 3) * 4) + 1}`,
+                  background: c(index)
+                }}
+              >
+                {index}
+              </div>
+            )
         ))
       }
     </div>
-  ))
-  .add('two-column', () => (
-    <div
-      style={{
-        padding: 8
-      }}
-      className='h-100 flex'
-    >
-      <div
-        style={{
-          padding: 8
-        }}
-        className='flex-none'
-      >
-        <div
-          className='bg-background-50 h-100'
-          style={{
-            width: 12 * 16
-          }}
-        />
-      </div>
-      <div
-        className='flex-auto'
-        style={{
-          padding: 8
-        }}
-      >
-        <div
-          className='bg-background-50 w-100 h-100'
-        />
-      </div>
-    </div>
-  ))
-  .add('nested columns', () => (
-    <div
-      style={{
-        padding: 8
-      }}
-      className='h-100 flex'
-    >
-      <div
-        style={{
-          padding: 8
-        }}
-        className='flex-none'
-      >
-        <div
-          className='bg-background-50 h-100'
-          style={{
-            width: 12 * 16
-          }}
-        />
-      </div>
-      <div
-        className='flex-auto'
-        style={{
-          padding: 8
-        }}
-      >
-        <div
-          className='bg-background-50 w-100 h-100'
-        >
-          <div className='w-100 h-100 flex flex-column'>
-            <div
-              style={{
-                padding: 8
-              }}
-              className='flex-none'
-            >
-              <div
-                className='bg-secondary-50 h-100'
-                style={{
-                  height: 12 * 16
-                }}
-              />
-            </div>
-            <div
-              className='relative flex-auto'
-              style={{
-                padding: 8
-              }}
-            >
-              <div
-                className='bg-primary-50 absolute top-0 bottom-0 left-0 right-0'
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  ))
+  )
+}
+
+function getGray (color) {
+  // const rgb = d3.rgb(color)
+  // const interim = hsluv.rgbToHsluv([rgb.r / 255, rgb.g / 255, rgb.b / 255])
+  // interim[1] = 0
+  // const output = hsluv.hsluvToRgb(interim).map((d) => d * 255)
+  // return `rgb(${output.join(', ')})`
+  const hcl = d3.hcl(color)
+  hcl.c = 0
+  return hcl.toString()
+}
